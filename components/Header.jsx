1c46c28c20e37e5030a5ff6b7b9c22792297c84a
Ahnaf-dev <ahnafbari99@gmail.com>
2023-10-08 13:49:41 -0600

    Update

---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cart from './Cart'
+import Link from 'next/link'
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container header__row">
+
+      <h2><Link href="/">Tea Shop Ecommerce</Link></h2>
+      <Cart/>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="container hero__container">
+
+      <h1>Discover the Perfect Brew</h1>
+      <p>Welcome to our enchanting tea emporium, where every sip is an invitation to savor tranquility. Delight in the world's finest loose-leaf teas, meticulously curated and artfully blended for tea aficionados like you. </p>
+      <button className="btn btn--white">See Our Products</button>
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
