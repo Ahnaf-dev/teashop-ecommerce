dcd5fbee5b307b023e33b52aebe76052820f74a4
Ahnaf-dev <ahnafbari99@gmail.com>
2023-10-08 13:55:32 -0600

    Update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="container hero__container">
 
       <h1>Discover the Perfect Brew</h1>
-      <p>Welcome to our enchanting tea emporium, where every sip is an invitation to savor tranquility. Delight in the world's finest loose-leaf teas, meticulously curated and artfully blended for tea aficionados like you. </p>
+      <p>Welcome to our enchanting tea emporium, where every sip is an invitation to savor tranquility. </p>
       <button className="btn btn--white">See Our Products</button>
 
       </div>
